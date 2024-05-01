39ce74aa61feca72220fce0f4ac716680bbf0b64
VinayakG311 <96966973+VinayakG311@users.noreply.github.com>
2024-05-01 23:20:54 +0530

    try

--- MC_Project/app/src/main/java/com/example/mc_project/MainActivity.kt
+++ MC_Project/app/src/main/java/com/example/mc_project/MainActivity.kt
@@ -13,18 +13,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mc_project.ui.theme.MC_ProjectTheme
+import com.facebook.react.modules.core.PermissionListener
 import org.jitsi.meet.sdk.JitsiMeet
 import org.jitsi.meet.sdk.JitsiMeetActivity
+import org.jitsi.meet.sdk.JitsiMeetActivityInterface
 import org.jitsi.meet.sdk.JitsiMeetConferenceOptions
 import java.net.URL
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(),JitsiMeetActivityInterface {
+
     var defaultOptions
-    = JitsiMeetConferenceOptions.Builder()
-    .setServerURL(URL("https://meet.jit.si"))
-    .setFeatureFlag("welcomepage.enabled", false)
-    .build();
-    ;
+            = JitsiMeetConferenceOptions.Builder()
+        .setServerURL(URL("https://meet.jit.si/"))
+        .setFeatureFlag("welcomepage.enabled", false)
+        .build();
+
     override fun onCreate(savedInstanceState: Bundle?) {
         JitsiMeet.setDefaultConferenceOptions(defaultOptions)
         super.onCreate(savedInstanceState)
@@ -32,16 +35,15 @@ class MainActivity : ComponentActivity() {
 
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    
+
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
                     Button(onClick = {
                         val options = JitsiMeetConferenceOptions.Builder()
-                            .setRoom("roomName") // Settings for audio and video
-                            //.setAudioMuted(true)
-                            //.setVideoMuted(true)
+                            .setRoom("room1") // Settings for audio and video
                             .build()
+
                         JitsiMeetActivity.launch(this, options);
                     }){
                         Text(text = "Create Room")
@@ -49,6 +51,10 @@ class MainActivity : ComponentActivity() {
                 }
 
         }
+    }
+
+    override fun requestPermissions(p0: Array<out String>?, p1: Int, p2: PermissionListener?) {
+        TODO("Not yet implemented")
     }
 }
 
