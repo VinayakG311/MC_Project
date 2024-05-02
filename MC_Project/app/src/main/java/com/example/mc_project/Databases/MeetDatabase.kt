5e1caf704fb9c2f3d700a26528f2db32fc3e9a40
VinayakG311 <96966973+VinayakG311@users.noreply.github.com>
2024-05-02 15:04:42 +0530

    dv

--- MC_Project/app/src/main/java/com/example/mc_project/Databases/MeetDatabase.kt
+++ MC_Project/app/src/main/java/com/example/mc_project/Databases/MeetDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 
 @Database(entities = [MeetEntity::class], version = 1, exportSchema = false)
 abstract class MeetDatabase: RoomDatabase() {
-    abstract fun userdao(): MeetDao
+    abstract fun meetdao(): MeetDao
 
     companion object {
 
